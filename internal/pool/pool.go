7715e0c1b6777612b0fecd0db3af1b2d8719e767
François-Xavier Wicht <wichtfx@protonmail.ch>
2022-05-05 11:35:11 +0200

    Refactored folder hierarchy

---
+++ internal/pool/pool.go
@@ -0,0 +1,31 @@
+package pool
+
+import "os"
+
+// Pool is an interface for a buffer pool
+type Pool interface {
+	PageSize() uint16
+	NewPage() *Page
+	GetPage(pageID uint16) *Page
+	EvictPage(pageID uint16) bool
+	EvictPages()
+	DeletePage(pageID uint16) error
+	UnpinPage(pageID uint16, dirty bool) error
+}
+
+type Bufferpool struct {
+	frames []*list
+	file   *os.File
+}
+
+func newBufferpool(file *os.File) *Bufferpool {
+	return &Bufferpool{file: file, frames: nil}
+}
+
+func (pool *Bufferpool) write(page *Page) error {
+	return nil
+}
+
+func (pool *Bufferpool) read(page *Page) error {
+	return nil
+}
