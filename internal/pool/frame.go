b43bd7fecbe1d9e887954b92f85fb9d40d2d7a16
François-Xavier Wicht <wichtfx@protonmail.ch>
2022-05-09 18:13:40 +0200

    Changed frame field/function/struct to private

--- internal/pool/frame.go
+++ internal/pool/frame.go
@@ -1,11 +1,9 @@
 package pool
 
-import "hbtrie/internal/kverrors"
-
 // Frame is a self-managed unit of the buffer pool. It consists in a double linked list of pages.
 // Each page, when queried, is pushed to the head of the list. Pages on the tail of the list are the least recently used.
 // Pages on the tail should thus be evicted first.
-type Frame struct {
+type frame struct {
 	// id    uint64
 	head  *Page
 	tail  *Page
@@ -15,25 +13,25 @@ type Frame struct {
 	allocation uint64
 }
 
-func (l *Frame) push(p *Page) {
+func (l *frame) push(p *Page) {
 	l.head.next.prev = p
 	p.next = l.head.next
 	p.prev = l.head
 	l.head.next = p
 }
 
-func (l *Frame) pop(p *Page) {
+func (l *frame) pop(p *Page) {
 	p.prev.next = p.next
 	p.next.prev = p.prev
 }
 
-func (l *Frame) boost(p *Page) {
+func (l *frame) boost(p *Page) {
 	l.pop(p)
 	l.push(p)
 }
 
-func NewFrame(allocation uint64) *Frame {
-	l := &Frame{
+func newFrame(allocation uint64) *frame {
+	l := &frame{
 		head:  NewPage(1),
 		tail:  NewPage(allocation),
 		pages: make(map[uint64]*Node),
@@ -46,7 +44,11 @@ func NewFrame(allocation uint64) *Frame {
 	return l
 }
 
-func (l *Frame) Query(id uint64) *Node {
+func (l *frame) query(id uint64) *Node {
+
+	if id < 1 {
+		return nil
+	}
 
 	if p, ok := l.pages[id]; ok {
 		l.boost(p.Page)
@@ -55,14 +57,14 @@ func (l *Frame) Query(id uint64) *Node {
 	return l.pages[id]
 }
 
-func (l *Frame) NewNode() (*Node, error) {
+func (l *frame) newNode() (node *Node, full bool) {
 	if l.cursor >= l.allocation-1 {
-		return nil, &kverrors.BufferOverflowError{Max: l.allocation, Cursor: l.cursor}
+		return nil, true
 	}
 	l.cursor++
 	page := NewPage(l.cursor)
 	n := &Node{Page: page}
 	l.pages[n.Id] = n
 	l.push(n.Page)
-	return n, nil
+	return n, false
 }
