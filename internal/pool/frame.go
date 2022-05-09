c66a1d6c61f6c2fb67b710a5871ca6397e70722f
François-Xavier Wicht <wichtfx@protonmail.ch>
2022-05-09 17:31:44 +0200

    Commenting of unnecessary fields

--- internal/pool/frame.go
+++ internal/pool/frame.go
@@ -2,11 +2,15 @@ package pool
 
 import "hbtrie/internal/kverrors"
 
+// Frame is a self-managed unit of the buffer pool. It consists in a double linked list of pages.
+// Each page, when queried, is pushed to the head of the list. Pages on the tail of the list are the least recently used.
+// Pages on the tail should thus be evicted first.
 type Frame struct {
-	head       *Page
-	tail       *Page
-	pages      map[uint64]*Node
-	dirties    map[uint64]*Node
+	// id    uint64
+	head  *Page
+	tail  *Page
+	pages map[uint64]*Node
+	// dirties    map[uint64]*Node
 	cursor     uint64
 	allocation uint64
 }
@@ -30,15 +34,15 @@ func (l *Frame) boost(p *Page) {
 
 func NewFrame(allocation uint64) *Frame {
 	l := &Frame{
-		head:    NewPage(1),
-		tail:    NewPage(allocation),
-		pages:   make(map[uint64]*Node),
-		dirties: make(map[uint64]*Node),
+		head:  NewPage(1),
+		tail:  NewPage(allocation),
+		pages: make(map[uint64]*Node),
+		// dirties: make(map[uint64]*Node),
 	}
 	l.allocation = allocation
 	l.head.next = l.tail
 	l.tail.prev = l.head
-	l.cursor = 2
+	l.cursor = 1
 	return l
 }
 
