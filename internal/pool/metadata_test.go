683a892e34350caaee46e1f89afce39cc979ff5b
François-Xavier Wicht <wichtfx@protonmail.ch>
2022-05-28 10:40:54 +0200

    Metadata simplification

--- internal/pool/metadata_test.go
+++ internal/pool/metadata_test.go
@@ -1,57 +1,32 @@
 package pool
 
 import (
-	"math/rand"
+	"math"
 	"testing"
 )
 
-func TestMarshalBinary(t *testing.T) {
+func TestMarshalUnmarshalMetadata(t *testing.T) {
 	meta := &metadata{}
-	meta.root = tuple{frame: 1, root: rand.Uint64()}
-	for i := 2; i < limit; i++ {
-		meta.children = append(meta.children, tuple{frame: uint64(i), root: rand.Uint64()})
-	}
-
+	meta.root = math.MaxUint64
+	meta.size = math.MaxUint64 - 4739
 	data, err := meta.MarshalBinary()
 	if err != nil {
-		t.Errorf("while marshaling metadata: %v", err)
+		t.Errorf("while marshalling: %v", err)
 		t.FailNow()
 	}
-
-	if len(data) != metaSize() {
-		t.Errorf("expected %d, got %d", metaSize(), len(data))
-		t.FailNow()
-	}
-
 	meta2 := &metadata{}
 	err = meta2.UnmarshalBinary(data)
 	if err != nil {
-		t.Errorf("while unmarshaling metadata: %v", err)
+		t.Errorf("while unmarshalling: %v", err)
+		t.FailNow()
+	}
+	if meta.root != meta2.root {
+		t.Errorf("expected %d, got %d", meta.root, meta2.root)
+		t.FailNow()
+	}
+	if meta.size != meta2.size {
+		t.Errorf("expected %d, got %d", meta.size, meta2.size)
 		t.FailNow()
 	}
 
-	if meta.root.frame != meta2.root.frame {
-		t.Errorf("expected %d, got %d", meta.root.frame, meta2.root.frame)
-		t.FailNow()
-	}
-	if meta.root.root != meta2.root.root {
-		t.Errorf("expected %d, got %d", meta.root.root, meta2.root.root)
-		t.FailNow()
-	}
-
-	if len(meta.children) != len(meta2.children) {
-		t.Errorf("expected %d, got %d", len(meta.children), len(meta2.children))
-		t.FailNow()
-	}
-
-	for i, child := range meta.children {
-		if child.frame != meta2.children[i].frame {
-			t.Errorf("expected %d, got %d", meta.children[i].frame, meta2.children[i].frame)
-			t.FailNow()
-		}
-		if child.root != meta2.children[i].root {
-			t.Errorf("expected %d, got %d", meta.children[i].root, meta2.children[i].root)
-			t.FailNow()
-		}
-	}
 }
