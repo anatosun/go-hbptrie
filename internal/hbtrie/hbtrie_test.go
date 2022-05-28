751404310b57d7e9534fcd47ff117d505c7989cb
François-Xavier Wicht <wichtfx@protonmail.ch>
2022-05-28 14:40:07 +0200

    HBTrie page eviction test

--- internal/hbtrie/hbtrie_test.go
+++ internal/hbtrie/hbtrie_test.go
@@ -2,37 +2,51 @@ package hbtrie
 
 import (
 	"crypto/sha1"
+	"crypto/sha512"
 	"hbtrie/internal/pool"
 	"math/rand"
+	"os"
 	"testing"
 )
 
 var store *HBTrieInstance
+var values map[[64]byte]uint64
 
 const size = 1000
 
 func TestInit(t *testing.T) {
 	store = NewHBPlusTrie(16, pool.NewBufferpool(nil, uint64(size)))
+	values = make(map[[64]byte]uint64)
+	h := sha512.New()
+
+	for i := 0; i < size; i++ {
+		h.Write([]byte{byte(i)})
+		key := [64]byte{}
+		copy(key[:32], h.Sum(nil)[:32])
+		copy(key[32:64], h.Sum(nil)[:32])
+		value := rand.Uint64()
+		values[key] = value
+	}
+
+	if store.Len() != 0 {
+		t.Errorf("expected size %d, got %d", 0, store.Len())
+		t.FailNow()
+	}
 }
 
 func TestInsertBelowChunkSize(t *testing.T) {
 	store = NewHBPlusTrie(16, pool.NewBufferpool(nil, uint64(size)))
 	step := 0
-	h := sha1.New()
 
-	for i := 0; i < size; i++ {
-		h.Write([]byte{byte(i)})
-		// key := make([]byte, 0, 16)
-		key := h.Sum(nil)[:16]
-		value := rand.Uint64()
-
-		err := store.Insert(key, value)
+	for key, value := range values {
+		k := key[:8]
+		err := store.Insert(k, value)
 		if err != nil {
 			t.Errorf("[step %d] while inserting to kv store(%d): %v", step, key, err)
 			t.FailNow()
 		}
 
-		v, err := store.Search(key)
+		v, err := store.Search(k)
 		if err != nil {
 			t.Errorf("[step %d] while searching for key '%v': %v", step, key, err)
 			t.FailNow()
@@ -48,21 +62,16 @@ func TestInsertBelowChunkSize(t *testing.T) {
 func TestInsertAboveChunkSize(t *testing.T) {
 	store = NewHBPlusTrie(16, pool.NewBufferpool(nil, uint64(size)))
 	step := 0
-	h := sha1.New()
 
-	for i := 0; i < size; i++ {
-		h.Write([]byte{byte(i)})
-		// key := make([]byte, 0, 40)
-		key := append(h.Sum(nil), h.Sum(nil)...)
-		value := rand.Uint64()
+	for key, value := range values {
 
-		err := store.Insert(key, value)
+		err := store.Insert(key[:], value)
 		if err != nil {
 			t.Errorf("[step %d] while inserting to kv store(%d): %v", step, key, err)
 			t.FailNow()
 		}
 
-		v, err := store.Search(key)
+		v, err := store.Search(key[:])
 		if err != nil {
 			t.Errorf("[step %d] while searching for key '%v': %v", step, key, err)
 			t.FailNow()
@@ -153,5 +162,52 @@ func TestUpdateKeys(t *testing.T) {
 			t.Errorf("[step %d] expected %v, got %v", step, value, v)
 			t.FailNow()
 		}
+	}
+}
+
+func TestInsertWithPageEviction(t *testing.T) {
+	filename := "test_insert_with_page_eviction"
+	file, err := os.Create(filename)
+	if err != nil {
+		t.Errorf("while creating file '%v': %v", filename, err)
+		t.FailNow()
+	}
+	t.Cleanup(func() {
+		file.Close()
+		os.Remove(filename)
+	})
+	store = NewHBPlusTrie(16, pool.NewBufferpool(file, uint64(5)))
+
+	step := 0
+	for key, value := range values {
+
+		err := store.Insert(key[:], value)
+		if err != nil {
+			t.Errorf("[step %d] while inserting to kv store(%d): %v", step, key, err)
+			t.FailNow()
+		}
+
+		v, err := store.Search(key[:])
+		if err != nil {
+			t.Errorf("[step %d] while searching for key '%v': %v", step, key, err)
+			t.FailNow()
+		}
+
+		if v != value {
+			t.Errorf("[step %d] expected %v, got %v", step, value, v)
+			t.FailNow()
+		}
+
+		step++
+	}
+	stat, err := os.Stat(filename)
+	if err != nil {
+		t.Errorf("while getting file stats: %v", err)
+		t.FailNow()
+
+	}
+	if stat.Size() == 0 {
+		t.Errorf("file size is 0")
+		t.FailNow()
 	}
 }
